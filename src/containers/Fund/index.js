40e3294432e1d20f6d99ce66f2b3272e44b97bfd
Peja <peja@relab.cc>
2022-01-20 13:40:21 +0800

    updated fund

--- src/containers/Fund/index.js
+++ src/containers/Fund/index.js
@@ -46,8 +46,8 @@ const Fund = () => {
           <tbody>
             {data.map(({ th, td }, i) => (
               <Box as="tr" color="prussianBlue" fontWeight="500" key={i}>
-                <Box borderBottom="0.5px solid" borderColor="brightGray" width="6em" px={responsive('1rem', '2.5rem')} py="1.4rem" textAlign="center" as="th" fontSize={responsive('1em', '3.2rem')}>{th}</Box>
-                <Box borderBottom="0.5px solid" borderColor="brightGray" px={responsive('1.8rem', '5rem')} fontSize={responsive('1.2rem', '2.4rem')} as="td">{td}</Box>
+                <Box borderBottom="1px solid" borderColor="brightGray" width="6em" px={responsive('1rem', '2.5rem')} py="1.4rem" textAlign="center" as="th" fontSize={responsive('1em', '3.2rem')}>{th}</Box>
+                <Box borderBottom="1px solid" borderColor="brightGray" px={responsive('1.8rem', '5rem')} fontSize={responsive('1.2rem', '2.4rem')} as="td">{td}</Box>
               </Box>
             ))}
           </tbody>
